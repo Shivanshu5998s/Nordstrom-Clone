1359a1d48a63aa560d05571a3f344c8889619c67
Shivanshu5998s <96103252+Shivanshu5998s@users.noreply.github.com>
2022-06-17 09:48:45 +0530

    Remove Data

--- Nordstromrack-Website-Clone-main/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, legacy_createStore } from "redux";
-import { dataReducer } from "./Data/Reducer";
-
-const enhancer = combineReducers({
-    Data: dataReducer
-})
-
-export const store = legacy_createStore(enhancer);
